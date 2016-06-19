2d10f34f4aba8c681c849a81d385d1e9569ee0e7
Lasse Diercks <lasse@jimdo.com>
2016-06-19 14:22:55 +0200

    a little usability addition to the gh-pages deploy script

--- deployToGithubPages.sh
+++ deployToGithubPages.sh
@@ -6,5 +6,8 @@ git co gh-pages
 #* get new changes from local master
 git rebase master
 
-#* commit and push to gh-pages
+#* push to gh-pages
 git push origin gh-pages
+
+#* go back to master
+git co master
